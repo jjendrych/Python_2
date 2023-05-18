757606499312521b4eb9772549d8848b60d48995
jjendrych <131374826+jjendrych@users.noreply.github.com>
2023-05-18 18:51:25 +0200

    Delete 2_StrukturyDanych directory

--- 2_StrukturyDanych/main2.py
+++
@@ -1,8 +0,0 @@
-#zadanie2
-print("Podaj swoje słowo:")
-word=input()
-word_backwards=word[::-1]
-if word==word_backwards:
-    print("Słowo jest palindromem")
-else:
-    print("słowo nie jest palindoromem")
